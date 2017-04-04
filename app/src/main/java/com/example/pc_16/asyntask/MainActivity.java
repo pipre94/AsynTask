fb38ad81121dc8f831509ff6ec0cf2fb1fcefe22
AndresTeran <pipre94@gmail.com>
2017-04-04 12:46:21 -0500

    se creo la clase onButtonStar se programa este metodo

--- app/src/main/java/com/example/pc_16/asyntask/MainActivity.java
+++ app/src/main/java/com/example/pc_16/asyntask/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pc_16.asyntask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,9 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+//se programa el onclick
     public void onButtonStart (){
+        new MyTask().execute();//cuando se da click al boton se ejecuta la tarea en segundo plano
+    }
 
+    public class MyTask extends AsyncTask<Integer, Integer, Integer>{
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            return null;
+        }
     }
 
 }
